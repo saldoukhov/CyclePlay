0829c195aa0ba42e06c1244ba1cae12f8d8c1dc7
Sergey Aldoukhov <saldoukhov@gmail.com>
2016-01-25 16:29:08 -0800

    Cycle

--- src/main.js
+++ src/main.js
@@ -1,2 +1,22 @@
-function foo() {
+import Cycle from '@cycle/core';
+import {div, label, input, hr, h1, makeDOMDriver} from '@cycle/dom';
+
+function main(sources) {
+    return {
+        DOM: sources.DOM.select('.myinput').events('input')
+            .map(ev => ev.target.value)
+            .startWith('')
+            .map(name =>
+                div([
+                    label('Name:'),
+                    input('.myinput', {attributes: {type: 'text'}}),
+                    hr(),
+                    h1(`Hello ${name}`)
+                ])
+            )
+    };
 }
+
+Cycle.run(main, {
+    DOM: makeDOMDriver('#main-container')
+});
